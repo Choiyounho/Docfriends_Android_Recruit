bfbc63c49d11b2250260949a8453514446122166
Choiyounho <maxosa78@gmail.com>
2021-11-21 15:59:31 +0900

    [feat] : 데이터 불러오기

--- solution/app/src/main/java/com/soten/solution/ui/home/HomeFragment.kt
+++ solution/app/src/main/java/com/soten/solution/ui/home/HomeFragment.kt
@@ -1,45 +1,52 @@
 package com.soten.solution.ui.home
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
+import android.util.Log
+import android.view.*
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.viewModels
 import com.soten.solution.R
 import com.soten.solution.databinding.FragmentHomeBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class HomeFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
-    private var _binding: FragmentHomeBinding? = null
+    private val homeViewModel by viewModels<HomeViewModel>()
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+    private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
-
+    ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        setHasOptionsMenu(true)
+        return binding.root
+    }
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        observeData()
+    }
+
+    private fun observeData() {
+        homeViewModel.docFriendsLiveData.observe(viewLifecycleOwner) {
+            Log.d("TestT", "진입")
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.home_menu, menu)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
 }
--- solution/app/src/main/java/com/soten/solution/ui/home/HomeViewModel.kt
+++ solution/app/src/main/java/com/soten/solution/ui/home/HomeViewModel.kt
@@ -1,13 +1,35 @@
 package com.soten.solution.ui.home
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.soten.data.api.response.DocFriendsResponse
+import com.soten.data.repository.DocFriendsRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class HomeViewModel : ViewModel() {
+@HiltViewModel
+class HomeViewModel @Inject constructor(
+    private val docFriendsRepository: DocFriendsRepository
+) : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+    private val _docFriendsLiveData = MutableLiveData<DocFriendsResponse>()
+    val docFriendsLiveData: LiveData<DocFriendsResponse> get() = _docFriendsLiveData
+
+    init {
+        initData()
     }
-    val text: LiveData<String> = _text
+
+    private fun initData() = viewModelScope.launch {
+        _docFriendsLiveData.value = docFriendsRepository.getConsultApi()
+        try {
+            Log.d("TestT", "data : ${_docFriendsLiveData.value}")
+        } catch (e: Exception) {
+            Log.d("TestT", "data : 실패")
+        }
+    }
+
 }
