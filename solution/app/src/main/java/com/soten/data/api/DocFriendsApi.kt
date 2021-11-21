44873276aedf910f937374f5f198bcd55ebbe2cc
Choiyounho <maxosa78@gmail.com>
2021-11-21 15:07:38 +0900

    [feat] : DocFriendsApi

--- solution/app/src/main/java/com/soten/data/api/DocFriendsApi.kt
+++ solution/app/src/main/java/com/soten/data/api/DocFriendsApi.kt
@@ -1,4 +1,11 @@
 package com.soten.data.api
 
+import com.soten.data.api.response.DocFriendsResponse
+import retrofit2.http.GET
+
 interface DocFriendsApi {
+
+    @GET("Docfriends_Android_Recruit/api/home.json")
+    suspend fun getConsultApi(): DocFriendsResponse
+
 }
